d9540569b2bd8f8f3ba7d988d724a164cd48e40e
Siim Talts <siim.talts@scoro.com>
2022-03-18 16:55:34 +0200

    added separate Exception class for API errors

--- src/ScoroRequestFailedException.php
+++ src/ScoroRequestFailedException.php
@@ -3,11 +3,30 @@ namespace ScoroAPI;
 
 class ScoroRequestFailedException extends \Exception {
 
+	private $response;
+	private $errors;
+
 	public function __construct($response, $errors) {
 		$message = json_encode([
 			'response' => $response,
 			'errors' => $errors,
 		]);
+		$this->response = $response;
+		$this->errors = $errors;
 		parent::__construct($message);
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getResponse() {
+		return $this->response;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getErrors() {
+		return $this->errors;
 	}
 }
